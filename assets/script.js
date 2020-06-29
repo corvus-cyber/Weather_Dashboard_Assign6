dc9e0e25f039f4a87627a2b9270ca21daf349bdc
Corvus <wyatta.hancock@gmail.com>
2020-06-29 13:58:17 -0600

    built a function for uv index to be shown, having trouble displaying

--- assets/script.js
+++ assets/script.js
@@ -36,35 +36,42 @@ $(document).ready(function(){
         $(".city").text(response.name);
         var weatherImg = "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
         $(".weather-image").attr("src", weatherImg);
-
         $(".wind").text("Wind Speed: " + response.wind.speed + " MPH");
         $(".humidity").text("Humidity: " + response.main.humidity + " %");
-        
         // Convert the temp to fahrenheit
         var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-
         // add temp content to html
         $(".tempF").text("Temperature (F) " + tempF.toFixed(2)) + " °F";
+        //Add latitude and longitude 
+        var latitude = response.coord.lat;
+        var longitude = response.coord.lon;
 
-        // Log the data in the console as well
-        console.log("Wind Speed: " + response.wind.speed + "MPH");
-        console.log("Humidity: " + response.main.humidity + "%");
-        console.log("Temperature (F): " + tempF + " °F");
+        //Here we are building the URL we need to query the UV index
+        var uvQueryUrl= "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=166a433c57516f51dfab1f7edaed8413";
+
+        //Here we run our uvindex Ajax call 
+        $.ajax({
+          url: uvQueryUrl,
+          method: "GET"
+        })
+          .then(function(response){
+            //add uvi index to html
+            var uv = response.current.uvi;
+            $("uv-container").text("UV Index: ");
+            console.log(uv);
+            $("#uv-text").text(uv);
+            if ("#uv-text" > 3){
+              $("#uv-container").addClass("good")
+            }
+            else if ("#uv-text" <7){
+              $("#uv-container").addClass("bad")
+            }
+            else {
+              $("#uv-container").addClass("ok")
+            } 
+          })
       });
-    //Here we are building the URL we need to query the UV index
-    var uvQueryUrl= "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=166a433c57516f51dfab1f7edaed8413";
 
-    //Here we run our uvindex Ajax call 
-    $.ajax({
-      url: uvQueryUrl,
-      method: "GET"
-    })
-    .then(function(response){
-      //add uvi index to html
-      var uv = response.current.uvi;
-      
-
-    })
   
   }
 
