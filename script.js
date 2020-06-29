c1a251145c9e75291086036eaccf59b86f525ab0
Corvus <wyatta.hancock@gmail.com>
2020-06-29 10:16:59 -0600

    cleaned up html, refined the queryURl

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ $(".search").on("click", function(){
     var APIKey = "166a433c57516f51dfab1f7edaed8413";
 
     // Here we are building the URL we need to query the database
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-      "q=Bujumbura,Burundi&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=166a433c57516f51dfab1f7edaed8413";
 
     // Here we run our AJAX call to the OpenWeatherMap API
     $.ajax({
